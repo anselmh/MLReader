150bc7f01a05f9d7625357f26a10896f744dc7c0
Anselm Hannemann <info@anselm-hannemann.com>
2013-02-09 19:18:41 +0100

    updates to inner code views
    htx to rodneyrehm
    
    Signed-off-by: Anselm Hannemann <info@anselm-hannemann.com>

--- mlr-inject.js
+++ mlr-inject.js
@@ -7,18 +7,31 @@
 	var boom = function () {
 	 
 		[].forEach.call( document.querySelectorAll('[data-markdown]'), function  fn(elem){
-		  
+console.log(elem.innerHTML, "--------------------------");
 			// strip leading whitespace so it isn't evaluated as code
 			var text      = elem.innerHTML.replace(/\n\s*\n/g,'\n\n');
 			var leadingws = text.match(/^\n?(\s*)/)[1].length;
 			var regex     = new RegExp('\\n?\\s{' + leadingws + '}','g');
 			var md        = (leadingws > 0) ? text.replace(regex,'\n') : text;
 
-			// convert entities to characters
-			var dummy = document.createElement("div");
-			dummy.innerHTML = md;
-			md = dummy.textContent;
 
+			// fix closing tag on new line, caused by dumb email clients
+			md = md.replace(/\n(<\/[^>]+>)/g, '$1\n');
+			
+			// fix URLs that were converted to <a> by dumb email clients
+			md = md.replace(/<a href="([^"]+)">\1<\/a>/g, '$1');
+
+			// convert leading &gt; (entity) to > (literal)
+			md = md.replace(/(^|\n)(&gt;\s*)+/g, function(match) {
+				// add trailing space to separate <i> injected by  
+				// dumb email clients to visualize a repliy-quote
+				return match.replace(/&gt;/g, '>') + ' ';
+			});
+
+			// fix whatver this is, you need 4 spaces!
+			md = md.replace(/\n\s{3}(\S&lt;)/g, '\n    $1');
+
+console.log(md, "--------------------------");
 			var html      = (new Showdown.converter()).makeHtml(md);
 		 
 			// here, have sum HTML
